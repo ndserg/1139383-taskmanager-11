362659d3aae7aeb8c7060c14368ff04a66757ea0
ndserg <ndserg@gmail.com>
2020-04-04 15:42:32 +0300

    2. Модули и инфраструктура проектов (устанавливает webpack и разбивает на мо дули)

---
+++ src/components/board.js
@@ -0,0 +1,7 @@
+export const createBoardTemplate = () => {
+  return (
+    `<section class="board container">
+      <div class="board__tasks"></div>
+    </section>`
+  );
+};
---
+++ src/components/main.js
@@ -0,0 +1,32 @@
+import {createSiteMenuTemplate} from "./components/site-menu.js";
+import {createSortingTemplate} from "./components/sorting";
+import {createFilterTemplate} from "./components/filter.js";
+import {createTaskTemplate} from "./components/task.js";
+import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
+import {createBoardTemplate} from "./components/board.js";
+
+const TASK_COUNT = 3;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
+
+const boardElement = siteMainElement.querySelector(`.board`);
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+
+render(boardElement, createSortingTemplate(), `afterbegin`);
+render(taskListElement, createTaskEditTemplate(), `beforeend`);
+
+for (let i = 0; i < TASK_COUNT; i++) {
+  render(taskListElement, createTaskTemplate(), `beforeend`);
+}
+
+render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
